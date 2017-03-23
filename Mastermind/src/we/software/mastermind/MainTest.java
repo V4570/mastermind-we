9d6d4d2502f70b35b4c81b9f1a167999abd5ea3c
Bill <vasilis.tos@gmail.com>
2017-03-23 19:26:18 +0200

    Update.

--- Mastermind/src/we/software/mastermind/MainTest.java
+++ Mastermind/src/we/software/mastermind/MainTest.java
@@ -8,6 +8,7 @@ public class MainTest {
 		System.out.println("Hello from the other siiiiiiide!");
 		System.out.println("I must have called a thousand tiiiiiiimes!");
 		System.out.println("Hacking intensifies");
+		System.out.println("Firewall intensifies");
 	}
 
 }
