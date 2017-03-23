a1f52522d00863ddf59a6653fdf1c66482213bcc
Bill <Bill@github.com>
2017-03-23 18:28:55 +0200

    Added two irrelevant lines.

---
+++ Mastermind/src/we/software/mastermind/MainTest.java
@@ -0,0 +1,12 @@
+package we.software.mastermind;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello from the other siiiiiiide!");
+		System.out.println("I must have called a thousand tiiiiiiimes!");
+	}
+
+}
